d4b76c274433cdfd131199f16a025c985cb87ed5
Frank <1716715+yeahoffline@users.noreply.github.com>
2018-01-31 12:07:18 +0100

    csv format

--- functions/analyticsToS3/index.js
+++ functions/analyticsToS3/index.js
@@ -2,7 +2,7 @@ const CsvExport = require('dynamodbexportcsv')
 let exporter = new CsvExport(process.env.AKEY, process.env.SKEY, 'us-east-1')
 
 exports.handle = function (e, ctx) {
-  exporter.exportTable('bk_analytics', ['userid', 'lastTs'], 1, false, 500, 'backup-dynamo-analytics', 'day' + new Date().getDay(), function (err) {
+  exporter.exportTable('bk_analytics', ['userid', 'lastTs'], 1, false, 500, 'backup-dynamo-analytics', `${new Date().getMonth() + 1}-${new Date().getDate()}`, function (err) {
     if (err) {
       console.log(err)
       return ctx.fail()
