b4b1d3561f2e8d1d25d269bde8d46d9ee38e91a2
Frank <1716715+yeahoffline@users.noreply.github.com>
2018-09-11 13:27:58 +0200

    optimize daily data

--- functions/getHistory/index.js
+++ functions/getHistory/index.js
@@ -51,67 +51,43 @@ exports.handle = async function (e, ctx) {
   if (allowedPrimaryCoins.indexOf(e.coin) === -1) return ctx.fail('Invalid coin')
   if (allowedSecCoins.indexOf(e.seccoin) === -1) return ctx.fail('Invalid seccoin')
 
+  const apiRes = {}
   const stack = []
   const hour = []
-  const week = []
-  const month = []
-  const quater = []
-  const halfyear = []
   const year = []
-  const startHourly = moment().subtract(2, 'days')
-  const startWeek = moment().subtract(1, 'week')
-  const startMonth = moment().subtract(1, 'month')
-  const startQuater = moment().subtract(3, 'month')
-  const startHalfyear = moment().subtract(6, 'month')
-  const startYear = moment().subtract(1, 'year')
   const end = moment()
-  const apiRes = {}
+  const startHourly = moment().subtract(2, 'days')
+  // const startYear = moment().subtract(1, 'year')
+  const startYear = moment().subtract(499, 'days')
 
   // last 48 hours
   while (startHourly.isBefore(end)) {
     hour.push(startHourly.startOf('hour').format('X'))
     startHourly.add(1, 'hour')
   }
-  // last 7 days + today -> 8 entries
-  while (startWeek.isBefore(end)) {
-    week.push(startWeek.startOf('day').format('X'))
-    startWeek.add(1, 'day')
-  }
-  // last month + today
-  while (startMonth.isBefore(end)) {
-    month.push(startMonth.startOf('day').format('X'))
-    startMonth.add(1, 'day')
-  }
-  // last quater + today
-  while (startQuater.isBefore(end)) {
-    quater.push(startQuater.startOf('day').format('X'))
-    startQuater.add(1, 'day')
-  }
-  // last half year + today -> 7 entries
-  while (startHalfyear.isBefore(end)) {
-    halfyear.push(startHalfyear.startOf('day').format('X'))
-    startHalfyear.add(1, 'month')
-  }
-  // last year + today -> 13 entries
-  while (startYear.isBefore(end)) {
+  while (startYear.isSameOrBefore(end, 'day')) {
     year.push(startYear.startOf('day').format('X'))
-    startYear.add(1, 'month')
+    startYear.add(1, 'day')
   }
 
   for (let c of coins) {
     const pair = `${c}_${c === e.coin ? e.seccoin : e.coin}`
 
     stack.push(getHistory('hourly', hourlyTableName, pair, hour))
-    stack.push(getHistory('weekly', dailyTableName, pair, week))
-    stack.push(getHistory('monthly', dailyTableName, pair, month))
-    stack.push(getHistory('quaterly', dailyTableName, pair, quater))
-    stack.push(getHistory('halfyearly', dailyTableName, pair, halfyear))
-    stack.push(getHistory('yearly', dailyTableName, pair, year))
+
+    // dynamodb batchGetItem max size per call is 100 items
+    stack.push(getHistory('daily', dailyTableName, pair, year.slice(0, 100)))
+    stack.push(getHistory('daily', dailyTableName, pair, year.slice(100, 200)))
+    stack.push(getHistory('daily', dailyTableName, pair, year.slice(200, 300)))
+    stack.push(getHistory('daily', dailyTableName, pair, year.slice(300, 400)))
+    stack.push(getHistory('daily', dailyTableName, pair, year.slice(400, 500)))
   }
+
   const rawData = await Promise.all(stack)
+
   for (let r of rawData) {
     if (apiRes[r.type] && apiRes[r.type][r.pair]) {
-      throw ctx.fail('rawData broken') // should never happen
+      apiRes[r.type][r.pair] = apiRes[r.type][r.pair].concat(r.data)
     } else if (apiRes[r.type]) {
       apiRes[r.type][r.pair] = r.data
     } else {
